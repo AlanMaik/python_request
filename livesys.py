425e08efaa01a3c2e268eb07322d9fe1d7f325dd
diario <diario>
2017-07-24 05:21:29 -0300

    Script para agilizar o processo de insercoa das tabelas

--- livesys.py
+++ livesys.py
@@ -1,23 +1,25 @@
-import MySQLdb, re
+import MySQLdb, re, os
 
 bitfarma = MySQLdb.connect(
     host = '127.0.0.1', 
     user = 'root', 
-    passwd = 'bitdemo123', 
+    passwd = '', 
     db = 'bitfarma', 
-    port = 9306)
+    port = 3306)
 livesys = MySQLdb.connect(
     host='127.0.0.1',
     user='root',
-    passwd='123',
+    passwd='',
     db='sf_bitfarma',
-    port = 5306)
+    port = 3306)
+def cls():
+
+    print("\n" * 100)
 
 def integracao_usuario_cab(tabela='', comentario='', versao_tipo='', versao_cod='', versao_build='', formato_filial='', tabela_referencia='', idgeral_possui='', codgeral_possui=''):
     """
         insert em dicionario_integracao_usuario_cab 
     """
-    print "||||||||| INTEGRAÇAO USUARIO CAB ||||||||||"
     print "Voce esta criando a INTEGRACAO DA TABELA"
     """
     tabela=raw_input("digite aqui o nome da TABELA: ") 
@@ -35,22 +37,22 @@ def integracao_usuario_cab(tabela='', comentario='', versao_tipo='', versao_cod=
 
 
 def integracao_usuario_cab_atributos(tabela,atributo_nome,referencia_atributo,atributo_tipo,atributo_tamanho,atributo_decimais,atributo_default,atributo_permitenull):
-
-    convertecampo_tipo = raw_input('diegite aqui convertecampo_tipo')
-    convertecampo_case1_condicao = raw_input('diegite aqui convertecampo_case1_condicao')
-    convertecampo_case1_valor = raw_input('diegite aqui convertecampo_case1_valor')
-    convertecampo_case2_condicao = raw_input('diegite aqui convertecampo_case2_condicao')
-    convertecampo_case2_valor = raw_input('diegite aqui convertecampo_case2_valor')
-    convertecampo_other_valor = raw_input('diegite aqui convertecampo_other_valor')
-    atributo_descricao = raw_input('diegite aqui atributo_descricao')
-    atributo_adicionafilial = raw_input('diegite aqui atributo_adicionafilial')
-    atributo_indice = raw_input('diegite aqui atributo_indice')
-    atributo_indicetipo = raw_input('diegite aqui atributo_indicetipo')
-    atributo_indicenome = raw_input('diegite aqui atributo_indicenome')
-    versao_tipo = raw_input('diegite aqui versao_tipo')
-    versao_cod = raw_input('diegite aqui versao_cod')
-    versao_build = raw_input('diegite aqui versao_build')
-    comentario = raw_input('diegite aqui comentario')
+    cls()
+    convertecampo_tipo = raw_input('Digite aqui convertecampo_tipo: ')
+    convertecampo_case1_condicao = raw_input('Digite aqui convertecampo_case1_condicao: ')
+    convertecampo_case1_valor = raw_input('Digite aqui convertecampo_case1_valor: ')
+    convertecampo_case2_condicao = raw_input('Digite aqui convertecampo_case2_condicao: ')
+    convertecampo_case2_valor = raw_input('Digite aqui convertecampo_case2_valor: ')
+    convertecampo_other_valor = raw_input('Digite aqui convertecampo_other_valor: ')
+    atributo_descricao = raw_input('Digite aqui atributo_descricao: ')
+    atributo_adicionafilial = raw_input('Digite aqui atributo_adicionafilial: ')
+    atributo_indice = raw_input('Digite aqui atributo_indice: ')
+    atributo_indicetipo = raw_input('Digite aqui atributo_indicetipo: ')
+    atributo_indicenome = raw_input('Digite aqui atributo_indicenome: ')
+    versao_tipo = raw_input('Digite aqui versao_tipo: ')
+    versao_cod = raw_input('Digite aqui versao_cod: ')
+    versao_build = raw_input('Digite aqui versao_build: ')
+    comentario = raw_input('Digite aqui comentario: ')
 
 
 
@@ -58,44 +60,59 @@ def integracao_usuario_cab_atributos(tabela,atributo_nome,referencia_atributo,at
 
     print query
 
+def inserir(result):
+    cls()
+    for i in result: 
+        os.system('cls' if os.name == 'nt' else 'clear')  
+        print "------VERIFICANDO OS ATRIBUTOS -------- \n" 
+        print "NOME: %s TIPO: %s COLLATION: %s NULL: %s CHAVE: %s" % (i[0], i[1], i[2], i[3], i[4])
+        add = raw_input("Adicionar este ATRIBUTO: ")
+        if(add=="s" or add=="S"):
+            rex = re.match('(decimal|DECIMAL)', i[1])
+            if(rex != None):
+                tipo_tamanho = i[1]
+                tipo = tipo_tamanho[:7]
+                tamanho = tipo_tamanho[7:]
+                tamanho = tamanho.replace('(',"")
+                tamanho = tamanho.replace(')',"") 
+                tamanho = tamanho.split(",")
+                tamanho_1 = tamanho[0]
+                tamanho_2 = tamanho[1]
+                
+                integracao_usuario_cab_atributos('acerto_',i[0],'',tipo,tamanho_1,tamanho_2,'00','1')
+                #print tipo, tamanho_x, tamanho[1]
+            elif():
+
+                pass
+            else:
+                #print '%s' % (i[1])
+                #print i[0], i[1], i[2], i[3], i[4]
+                 pass
+        elif(add=="n" or add=="N" or add==""):
+            continue
+        else:
+            print "não entendi! estou parando!"
+            break
+    cls()
+    main()        
+def main():    
+    print "######## OLA BEM VINDO AO PROGRAMA DE INSERCAO DAS TABELAS ########"
+    print ""
+    print ""
+
+    d = bitfarma.cursor()
+    tabela_bitfarma = raw_input("Digite o nome da tabela do programa que usara agora:")
+    query1 = "SHOW FULL columns FROM %s FROM bitfarma" % (tabela_bitfarma)
+    d.execute(query1)
+    result = d.fetchall()
+    inserir(result)
 
 
-    
-d = bitfarma.cursor()
-
-tabela_bitfarma = "acerto_01"
-query1 = "SHOW FULL columns FROM %s FROM bitfarma" % (tabela_bitfarma)
-d.execute(query1)
-result = d.fetchall()
-
-for i in result:
-    #SQL_DELETED     SQL_ROWID
-    rex = re.match('(decimal|DECIMAL)', i[1])
-    if(rex != None):
-        tipo_tamanho = i[1]
-        tipo = tipo_tamanho[:7]
-        tamanho = tipo_tamanho[7:]
-        tamanho = tamanho.replace('(',"")
-        tamanho = tamanho.replace(')',"") 
-        tamanho = tamanho.split(",")
-        tamanho_1 = tamanho[0]
-        tamanho_2 = tamanho[1]
-        print i[0], i[1], i[2], i[3], i[4]
-        integracao_usuario_cab_atributos('acerto_',i[0],'',tipo,tamanho_1,tamanho_2,'00','1')
-
-        #print tipo, tamanho_x, tamanho[1]
-    elif():
-
-        pass
-    else:
-        #print '%s' % (i[1])
-         #print i[0], i[1], i[2], i[3], i[4]
-         pass
-            
   
 
 #SQL_DELETED 
 #SQL_ROWID  
         
-#if __name__ == '__main__':
+if __name__ == '__main__':
+    main()
 #integracao_usuario_cab(tabela_referencia='teste')
